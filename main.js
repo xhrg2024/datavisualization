2c6624dbfc5bc64e40200a1efc0db935a80e7abf
L06z24y <2300016606@stu.pku.edu.cn>
2026-06-01 02:04:49 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const chartRegistry = {
 
 const moduleSpecs = [
   { module: 'macro', src: './modules/memberA/page.html', title: '引子 · 谁在赢得诺贝尔奖？', dataFile: './data/memberA_data.json' },
-  { module: 'trajectory', src: './modules/memberB/page.html', title: '蓄力 · 卓越是如何炼成的？', dataFile: './data/memberB_data.json' },
+  { module: 'trajectory', src: './modules/memberB/page.html', title: '蓄力 · 卓越是如何炼成的？', dataFile: './data/memberB/memberB_data.json' },
   { module: 'network', src: './modules/memberC/page.html', title: '同行 · 巨人的肩膀与同行者', dataFile: './data/memberC_data.json' },
   { module: 'alluvial', src: './modules/memberD/page.html', title: '破壁 · 他们在研究什么？', dataFile: './data/memberD_data.json' },
   { module: 'morphing', src: './modules/memberE/page.html', title: '余音 · 改变世界的代表作', dataFile: './data/memberE_data.json' }
